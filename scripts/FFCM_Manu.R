90236afadc7958bb2378c188ff04f9c00bab87d9
Attanasio <jattanasio@nps.gov>
2025-12-09 12:15:07 -0500

    made an unreliable column and clean dataset

--- scripts/FFCM_Manu.R
+++ scripts/FFCM_Manu.R
@@ -457,4 +457,126 @@ ggplot(data, aes(x = Species, y = Growth)) +
 #make sure I got all the seedlings that should be altered. Can do this when I deal with zombie seedlings?
 write_xlsx(data, 'C:\\Users\\jattanasio\\OneDrive - DOI\\Desktop\\R_related\\FFCM\\future_forests_analysis_2025\\data\\data.xlsx')
 
+# Continuing to alter the data files into what I need
+# added measure (what type of data, sample.period (season), year)
 
+longdat <- read_excel("data/data_long.xlsx") %>% 
+  rename(sapling.id = UniqueID, planting.date = PlantingDate) %>% 
+  rename_with(tolower)
+
+longdat2 <- longdat %>%
+  pivot_wider(names_from = visit, values_from = data) %>%
+# getting the growth in the fall since the summer of 2019. So total growth from the beginning at the end of the growing season.
+# Unsure if I should also be doing this for the spring
+  mutate(TotalGrowth_fall2019 = Length_fall2019 - Length_summer2019) %>%
+  mutate(TotalGrowth_fall2020 = Length_fall2020 - Length_summer2019) %>%
+  mutate(TotalGrowth_fall2021 = Length_fall2021 - Length_summer2019) %>%
+  mutate(TotalGrowth_fall2022 = Length_fall2022 - Length_summer2019) %>%
+  mutate(TotalGrowth_fall2023 = Length_fall2023 - Length_summer2019) %>%
+  mutate(TotalGrowth_fall2024 = Length_fall2024 - Length_summer2019) %>%
+  pivot_longer(
+    cols = starts_with(c("Length", "Browse", "Live", "Total")),
+    names_to = "visit",
+    values_to = "data") %>%
+  mutate(
+    measure = case_when(
+      str_detect(visit, "Length") ~ "length",
+      str_detect(visit, "Browse") ~ "browse",
+      str_detect(visit, "LiveDead") ~ "livedead",
+      str_detect(visit, "Total") ~ "growth"
+    )
+  ) %>%
+  mutate(
+    sample.period = case_when(
+      str_detect(visit, "fall") ~ "fall",
+      str_detect(visit, "summer") ~ "summer"
+    )
+  ) %>%
+  mutate(
+    year = case_when (
+      str_detect(visit, "2019") ~ "2019",
+      str_detect(visit, "2020") ~ "2020",
+      str_detect(visit, "2021") ~ "2021",
+      str_detect(visit, "2022") ~ "2022",
+      str_detect(visit, "2023") ~ "2023",
+      str_detect(visit, "2024") ~ "2024",
+    )
+  ) %>%
+  select(-visit) %>%
+  pivot_wider(names_from = measure, values_from = data) %>% 
+  mutate(region = case_when(
+    species == "tulip" | species == "s.gum" ~ "southern",
+    species == "r.oak" | species == "w.spruce" | species == "w.pine" ~ "local",
+    species == "ch.oak" | species == "r.cedar" | species == "w.oak" ~ "maine"), .before = tube)
+
+# Now I am looking for zombie seedlings aka the seedlings that were at one point marked dead and then later alive
+# Important to remember that Belfast was not visited in the fall of 2020, so excluding Belfast 
+
+zombie <- longdat %>%
+  pivot_wider(names_from = visit, values_from = data) %>%
+  select(sapling.id, site, species, starts_with("LiveDead"))
+
+zombie1 <- zombie %>%
+  filter(LiveDead_fall2019 == 0 & LiveDead_summer2020 == 1)
+
+write_xlsx(zombie1, 'C:\\Users\\jattanasio\\OneDrive - DOI\\Desktop\\R_related\\FFCM\\future_forests_analysis_2025\\data\\zombie1.xlsx')
+
+zombie2 <- zombie %>%
+  filter(LiveDead_summer2020 == 0 & LiveDead_fall2020 == 1) %>%
+  filter(site != "Belfast")
+
+write_xlsx(zombie2, 'C:\\Users\\jattanasio\\OneDrive - DOI\\Desktop\\R_related\\FFCM\\future_forests_analysis_2025\\data\\zombie2.xlsx')
+
+zombie3 <- zombie %>%
+  filter(LiveDead_fall2020 == 0 & LiveDead_summer2021 == 1) %>%
+  filter(site != "Belfast")
+
+write_xlsx(zombie3, 'C:\\Users\\jattanasio\\OneDrive - DOI\\Desktop\\R_related\\FFCM\\future_forests_analysis_2025\\data\\zombie3.xlsx')
+
+zombie4 <- zombie %>%
+  filter(LiveDead_summer2021 == 0 & LiveDead_fall2021 == 1) 
+
+write_xlsx(zombie4, 'C:\\Users\\jattanasio\\OneDrive - DOI\\Desktop\\R_related\\FFCM\\future_forests_analysis_2025\\data\\zombie4.xlsx')
+
+zombie5 <- zombie %>%
+  filter(LiveDead_fall2021 == 0 & LiveDead_summer2022 == 1) 
+
+write_xlsx(zombie5, 'C:\\Users\\jattanasio\\OneDrive - DOI\\Desktop\\R_related\\FFCM\\future_forests_analysis_2025\\data\\zombie5.xlsx')
+
+zombie6 <- zombie %>%
+  filter(LiveDead_summer2022 == 0 & LiveDead_fall2022 == 1)
+write_xlsx(zombie6, 'C:\\Users\\jattanasio\\OneDrive - DOI\\Desktop\\R_related\\FFCM\\future_forests_analysis_2025\\data\\zombie6.xlsx')
+
+zombie7 <- zombie %>%
+  filter(LiveDead_fall2022 == 0 & LiveDead_summer2023 == 1)
+
+write_xlsx(zombie7, 'C:\\Users\\jattanasio\\OneDrive - DOI\\Desktop\\R_related\\FFCM\\future_forests_analysis_2025\\data\\zombie7.xlsx')
+
+zombie8 <- zombie %>%
+  filter(LiveDead_summer2023 == 0 & LiveDead_fall2023 == 1)
+
+write_xlsx(zombie8, 'C:\\Users\\jattanasio\\OneDrive - DOI\\Desktop\\R_related\\FFCM\\future_forests_analysis_2025\\data\\zombie8.xlsx')
+
+zombie9 <- zombie %>%
+  filter(LiveDead_fall2023 == 0 & LiveDead_summer2024 == 1)
+
+write_xlsx(zombie9, 'C:\\Users\\jattanasio\\OneDrive - DOI\\Desktop\\R_related\\FFCM\\future_forests_analysis_2025\\data\\zombie9.xlsx')
+
+zombie10 <- zombie %>%
+  filter(LiveDead_summer2024 == 0 & LiveDead_fall2024 == 1)
+
+write_xlsx(zombie10, 'C:\\Users\\jattanasio\\OneDrive - DOI\\Desktop\\R_related\\FFCM\\future_forests_analysis_2025\\data\\zombie10.xlsx')
+# 462 zombie seedlings. We may want to exclude these going forward
+
+# Making a new column called unreliable
+# Also I see there are a lot of NAs in the browse column, I think I should make those 0? 
+
+clean <- longdat2 %>%
+  mutate(unreliable = 0)
+
+# There are 462 seedlings that are unreliable, I couple copy paste in their sapling.id but I will double check
+
+#t <- clean %>% 
+#  select(-c(year, browse, livedead)) %>% 
+# pivot_wider(names_from = sample.period, values_from = length) %>% 
+# mutate(growth = fall2024 - summer2019)
